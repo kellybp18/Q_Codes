2ebeb84e09e567f4a2b82f58d63c7df076483932
Brian Kelly <bkelly2014@ufl.edu>
2024-10-22 13:33:34 -0400

    Added figure for initial Qs model

--- plot_xsections.py
+++ plot_xsections.py
@@ -5,6 +5,7 @@ import os
 import time
 
 map_coords = np.array([-73.0,-68.0,-34.0,-29.0])
+data_dir = '/Volumes/External/Tomography/'
 
 lonmin = -73.0
 lonmax = -70.0
@@ -12,10 +13,10 @@ depmin = 10
 depmax = -72
 
 q_database = pd.read_csv('/Volumes/External/Attenuation/q_database.csv')
-qs_model = pd.read_csv('/Volumes/External/Tomography/qs_model.csv')
+qs_model = pd.read_csv(data_dir + 'qs_model.csv')
 elev_data = pd.read_table('/Users/bpk/Documents/AGU_2021/Illapel_topo15.xyz',sep='\t',dtype=float,names=['lon','lat','elev'])
 stn_data = pd.read_table('/Users/bpk/Documents/BPK_Masters_2019/AGU_Fall_Meeting_2019/Illapel_Stns.gmt',sep=' ',dtype=float,usecols=[0,1,2],names=['lat','lon','elev'])
-qs_initial_model = np.loadtxt('/Volumes/External/Tomography/m0.txt')
+qs_initial_model = (1/450)*np.ones(90160)#np.loadtxt(data_dir + 'm0.txt')
 
 qs_model.loc[(qs_model['Qs'] > 0.0) & (qs_model['Qs'] < 75.0),'Qs'] = 75.0
 qs_model.loc[(qs_model['Qs'] > 1500.0),'Qs'] = 1500.0
@@ -40,7 +41,7 @@ print(surflonstep,surfdepstep)
 
 hitcounts = pd.DataFrame({'boxnum':[],'hitcount':[],'total_ray_dist':[]})
 
-dist = np.loadtxt('/Volumes/External/Tomography/dist.txt')
+dist = np.loadtxt(data_dir + 'dist.txt')
 
 for j in range(len(dist[:,0])):
    blockdist = dist[j,:]
@@ -49,10 +50,10 @@ for j in range(len(dist[:,0])):
    totaldist = np.sum(nonzerodists)
    hitcounts = hitcounts.append({'boxnum':j+1,'hitcount':hitcount,'total_ray_dist':totaldist},ignore_index=True)
 
-hitcounts.to_csv('/Volumes/External/Tomography/hitcounts.csv',index=False)
+hitcounts.to_csv(data_dir + 'hitcounts.csv',index=False)
 #hitcounts = pd.read_csv('/Volumes/External/Tomography/hitcounts.csv')
 
-os.system('cd /Volumes/External/Tomography/Figures')
+os.system('cd ' + data_dir + 'Figures')
 
 for i in uniqlats:
     latslice = qs_model[qs_model['lat'] == i]
@@ -62,6 +63,16 @@ for i in uniqlats:
         continue
     currentqmin = np.min(latslice['Qs'])
     lat_round = np.round(i,2)
+
+    mask_data = pd.DataFrame(latslice.loc[:,['lon','dep','Qs']])
+    mask_data.to_csv((data_dir + 'Figures/maskgrid_'+str(lat_round)+'.xyz'),header=None,index=None,sep=' ',mode='w')
+
+    os.system(('gmt blockmean ' + data_dir + 'Figures/maskgrid_'+str(lat_round)+'.xyz -R-73/-68/-180/0 -I'+str(surflonstep)+'/'+str(surfdepstep)+' > ' + data_dir + 'Figures/maskgridmed_'+str(lat_round)+'.xyz'))
+    time.sleep(3)
+    os.system(('gmt surface ' + data_dir + 'Figures/maskgridmed_'+str(lat_round)+'.xyz -G' + data_dir + 'Figures/maskgridmed_'+str(lat_round)+'.grd -I'+str(surflonstep)+'/'+str(surfdepstep)+' -Lu1500.0 -R-73/-68/-180/0 -Tb1i0'))
+    time.sleep(5)
+    os.system(('gmt grdclip ' + data_dir + 'Figures/maskgridmed_'+str(lat_round)+'.grd -G' + data_dir + 'Figures/maskgridclip_'+str(lat_round)+'.grd -Sb75.0/NaN'))
+    time.sleep(5)
 
     latslice_boxnums = np.array(latslice['box_num'])
     for lbox in latslice_boxnums:
@@ -84,31 +95,33 @@ for i in uniqlats:
     # Get data for station plotting
 
     surf_data = pd.DataFrame(latslice.loc[:,['lon','dep','Qs']])
-    surf_data.to_csv(('/Volumes/External/Tomography/Figures/surfgrid_'+str(lat_round)+'.xyz'),header=None,index=None,sep=' ',mode='w')
+    surf_data.to_csv((data_dir + 'Figures/surfgrid_'+str(lat_round)+'.xyz'),header=None,index=None,sep=' ',mode='w')
 
-    os.system(('gmt blockmean /Volumes/External/Tomography/Figures/surfgrid_'+str(lat_round)+'.xyz -R-73/-68/-180/0 -I'+str(surflonstep)+'/'+str(surfdepstep)+' > /Volumes/External/Tomography/Figures/surfgridmed_'+str(lat_round)+'.xyz'))
+    os.system(('gmt blockmean ' + data_dir + 'Figures/surfgrid_'+str(lat_round)+'.xyz -R-73/-68/-180/0 -I'+str(surflonstep)+'/'+str(surfdepstep)+' > ' + data_dir + 'Figures/surfgridmed_'+str(lat_round)+'.xyz'))
     time.sleep(3)
-    os.system(('gmt surface /Volumes/External/Tomography/Figures/surfgridmed_'+str(lat_round)+'.xyz -G/Volumes/External/Tomography/Figures/surfgridmed_'+str(lat_round)+'.grd -I'+str(surflonstep)+'/'+str(surfdepstep)+' -Lu1500.0 -R-73/-68/-180/0 -Tb1i0'))
+    os.system(('gmt surface ' + data_dir + 'Figures/surfgridmed_'+str(lat_round)+'.xyz -G' + data_dir + 'Figures/surfgridmed_'+str(lat_round)+'.grd -I'+str(surflonstep)+'/'+str(surfdepstep)+' -Lu1500.0 -R-73/-68/-180/0 -Tb1i0'))
     time.sleep(5)
+    os.system(('gmt grdmath ' + data_dir + 'Figures/surfgridmed_'+str(lat_round)+'.grd ' + data_dir + 'Figures/maskgridclip_'+str(lat_round)+'.grd OR = ' + data_dir + 'Figures/surfmaskgridmed_'+str(lat_round)+'.grd'))
+    time.sleep(3)
     print("Okay")
 
     fig = pygmt.Figure()
     pygmt.xyz2grd(x=np.ascontiguousarray(latslice_nozero['lon']),
                 y=np.ascontiguousarray(latslice_nozero['dep']),
                 z=np.ascontiguousarray(latslice_nozero['Qs']),
-                outgrid = ('/Volumes/External/Tomography/Figures/Q_'+str(lat_round)+'.grd'),
+                outgrid = (data_dir + 'Figures/Q_'+str(lat_round)+'.grd'),
                 region=[uniqlons[0],uniqlons[-1],uniqdeps[0],uniqdeps[-1]],
                 spacing=(str(lonstep)+'+e/'+str(depstep)+'+e'))
-    pygmt.grdclip(('/Volumes/External/Tomography/Figures/surfgridmed_'+str(lat_round)+'.grd'),
-                outgrid=('/Volumes/External/Tomography/Figures/surfgridclip_'+str(lat_round)+'.grd'),
-                below=[75,np.nan])
+    pygmt.grdclip((data_dir + 'Figures/surfmaskgridmed_'+str(lat_round)+'.grd'),
+                outgrid=(data_dir + 'Figures/surfgridclip_'+str(lat_round)+'.grd'),
+                below=[75,75.0])
     #pygmt.config(COLOR_NAN='white')
     #pygmt.makecpt(cmap='seis',
     #            series=[q_min,q_max,q_inc],
     #            continuous=True,
     #            background='o')
-    fig.grdimage(('/Volumes/External/Tomography/Figures/Q_'+str(lat_round)+'.grd'),
-                cmap='/Volumes/External/Tomography/Figures/qs.cpt',
+    fig.grdimage((data_dir + 'Figures/Q_'+str(lat_round)+'.grd'),
+                cmap=data_dir + 'Figures/qs.cpt',
                 projection='x4.0/0.06',
                 region=[lonmin,lonmax,depmax,depmin],
                 interpolation='n')
@@ -116,10 +129,10 @@ for i in uniqlats:
                 region=[lonmin,lonmax,depmax,depmin],
                 frame=['WSne','xa1f0.5+lLongitude','ya10f10+l"Depth (km)"'],
                 )
-    fig.colorbar(frame=['xc/Volumes/External/Tomography/Figures/cbar_annots.txt+LQs'],
-                 cmap='/Volumes/External/Tomography/Figures/qs.cpt',
-                 position='JMR+o0.75c/0c+w7c/0.5c+n"No Data"')
-    fig.savefig(('/Volumes/External/Tomography/Figures/'+str(lat_round)+'_slice.png'))
+    fig.colorbar(frame=['xc' + data_dir + 'Figures/cbar_annots.txt+LQs'],
+                 cmap=data_dir + 'Figures/qs.cpt',
+                 position='JMR+o0.5c/0c+w4.92c/0.4c+n"No Data"')
+    fig.savefig((data_dir + 'Figures/'+str(lat_round)+'_slice.png'))
 
     fig2 = pygmt.Figure()
     #pygmt.makecpt(cmap='seis',
@@ -128,30 +141,35 @@ for i in uniqlats:
     #            background='o')
     pygmt.grdtrack(points='',
                 grid='/Users/bpk/Documents/AGU_2021/Illapel_topo15.grd',
-                outfile='/Volumes/External/Tomography/Figures/topo_track.txt',
+                outfile=data_dir + 'Figures/topo_track.txt',
                 region=[lonmin,lonmax,depmax,depmin],
                 profile=('-73/' + str(lat_round) + '/-68/' + str(lat_round) + '+i0.1'))
-    topo_track = pd.read_table('/Volumes/External/Tomography/Figures/topo_track.txt',sep='\t',dtype=float,names=['lon','lat','dep'])
+    topo_track = pd.read_table(data_dir + 'Figures/topo_track.txt',sep='\t',dtype=float,names=['lon','lat','dep'])
 #    os.system("awk '{print $1,($3/1000)}' </Volumes/External/Tomography/Figures/topo_track.txt> /Volumes/External/Tomography/Figures/topoline.xy")
 #    time.sleep(2)
     pygmt.grdtrack(points='',
-                grid='/Users/bpk/Documents/BPK_Masters_2019/Illapel_Map/Illapel_Slab2_Contours.grd',
-                outfile='/Volumes/External/Tomography/Figures/slab_track.txt',
+                grid='/Users/bpk/Documents/AGU_2021/Illapel_Slab2_Surface.grd',
+                outfile=data_dir + 'Figures/slab_track.txt',
                 region=[lonmin,lonmax,depmax,depmin],
-                profile=('-73/' + str(lat_round) + '/-68/' + str(lat_round) + '+i0.1'),
+                profile=('-72.7/' + str(lat_round) + '/-68/' + str(lat_round) + '+i0.1'),
                 interpolation='n',
                 skiprows=True)
-    slab = pd.read_table('/Volumes/External/Tomography/Figures/slab_track.txt',sep='\t',dtype=float,names=['lon','lat','dep'])
-    first_row = pd.DataFrame({'lon':[-73.0],'lat':[lat_round],'dep':[(topo_track['dep'][0]/1000)]},index=[0])
-    slab = pd.concat([first_row,slab]).reset_index(drop = True)
+    slab = pd.read_table(data_dir + 'Figures/slab_track.txt',sep='\t',dtype=float,names=['lon','lat','dep'])
+    #first_row = pd.DataFrame({'lon':[-73.0],'lat':[lat_round],'dep':[(topo_track['dep'][0]/1000)]},index=[0])
+    #slab = pd.concat([first_row,slab]).reset_index(drop = True)
     print(slab.head())
 #    os.system("awk '{print $1,$3}' </Volumes/External/Tomography/Figures/slab_track.txt> /Volumes/External/Tomography/Figures/slabline.xy")
 #    time.sleep(2)
-    fig2.grdimage(('/Volumes/External/Tomography/Figures/surfgridclip_'+str(lat_round)+'.grd'),
+    fig2.grdimage((data_dir + 'Figures/surfgridclip_'+str(lat_round)+'.grd'),
                 projection='x4.0/0.06',
-                cmap='/Volumes/External/Tomography/Figures/qs_surf.cpt',
+                cmap=data_dir + 'Figures/qs_surf.cpt',
                 region=[lonmin,lonmax,depmax,depmin],
                 interpolation='n')
+    fig2.grdcontour(grid=(data_dir + 'Figures/surfgridclip_'+str(lat_round)+'.grd'),
+                interval=200,
+                annotation='400+f5p',
+                projection='x4.0/0.06',
+                region=[lonmin,lonmax,depmax,depmin])
     fig2.basemap(projection='x4.0/0.06',
                 region=[lonmin,lonmax,depmax,depmin],
                 frame=['WSne','xa1f1+lLongitude','ya10f5+l"Depth (km)"'],
@@ -176,23 +194,33 @@ for i in uniqlats:
                 style='t0.2',
                 color='cyan',
                 pen='thin,black')
-    fig2.colorbar(frame=['xc/Volumes/External/Tomography/Figures/cbar_annots.txt+LQs'],
-                 cmap='/Volumes/External/Tomography/Figures/qs.cpt',
-                 position='JMR+o0.75c/0c+w7c/0.5c')
-    fig2.savefig(('/Volumes/External/Tomography/Figures/'+str(lat_round)+'_slice_surf.png'))
+    if i == uniqlats[np.argmin(np.abs(uniqlats - (-31.57)))]:
+        fig2.plot(data='/Users/bpk/Documents/BPK_Masters_2019/Illapel_Events/main_shock.loc',
+                projection='x4.0/0.06',
+                region=[lonmin,lonmax,depmax,depmin],
+                style='a0.4',
+                color='yellow',
+                pen='thin,black',
+                incols=[1,2]
+                )
+    fig2.colorbar(frame=['xc' + data_dir + 'Figures/cbar_annots.txt+LQs'],
+                 cmap=data_dir + 'Figures/qs.cpt',
+                 position='JMR+o0.5c/0c+w4.92c/0.4c+n"No Data"')
+    fig2.savefig((data_dir + 'Figures/'+str(lat_round)+'_slice_surf.png'))
 
     fig3 = pygmt.Figure()
     pygmt.xyz2grd(x=np.array(hitcount_data['lon']),
                 y=np.array(hitcount_data['dep']),
                 z=np.array(hitcount_data['total_ray_dist']),
-                outgrid = ('/Volumes/External/Tomography/Figures/hitcounts_'+str(lat_round)+'.grd'),
+                outgrid = (data_dir + 'Figures/hitcounts_'+str(lat_round)+'.grd'),
                 region=[uniqlons[0],uniqlons[-1],uniqdeps[0],uniqdeps[-1]],
                 spacing=(str(lonstep)+'+e/'+str(depstep)+'+e'))
-    pygmt.makecpt(cmap='seis',
+    pygmt.makecpt(cmap=data_dir + 'Figures/YlOrRd.cpt',
                 series=(str(mindist)+'/'+str(maxdist)+'/'+str((maxdist-mindist)/30)),
                 continuous=True,
+                reverse=True,
                 background='o')
-    fig3.grdimage(('/Volumes/External/Tomography/Figures/hitcounts_'+str(lat_round)+'.grd'),
+    fig3.grdimage((data_dir + 'Figures/hitcounts_'+str(lat_round)+'.grd'),
                 projection='x4.0/0.06',
                 region=[lonmin,lonmax,depmax,depmin],
                 interpolation='n')    
@@ -203,8 +231,30 @@ for i in uniqlats:
     fig3.text(x=np.array(hitcount_data['lon']),
               y=np.array(hitcount_data['dep']),
               text=np.array(np.array(hitcount_data['hitcount'],dtype=int),dtype=str),
+              font="7p,Helvetica,black",
               projection='x4.0/0.06',
               region=[lonmin,lonmax,depmax,depmin])
     fig3.colorbar(frame=['x+l"Sum of Ray Distances in Box"','y+lkm'],
-                 position='JMR+o0.75c/0c+w7c/0.5c')
-    fig3.savefig(('/Volumes/External/Tomography/Figures/'+str(lat_round)+'_hitcounts.png'))
+                 position='JMR+o0.5c/0c+w4.92c/0.4c')
+    fig3.savefig((data_dir + 'Figures/'+str(lat_round)+'_hitcounts.png'))
+
+    fig4 = pygmt.Figure()
+    pygmt.xyz2grd(x=np.ascontiguousarray(latslice['lon']),
+                y=np.ascontiguousarray(latslice['dep']),
+                z=1/qs_initial_model[latslice_boxnums.astype(int)],
+                outgrid = (data_dir + 'Figures/Q_synth'+str(lat_round)+'.grd'),
+                region=[uniqlons[0],uniqlons[-1],uniqdeps[0],uniqdeps[-1]],
+                spacing=(str(lonstep)+'+e/'+str(depstep)+'+e'))
+    fig4.grdimage((data_dir + 'Figures/Q_synth'+str(lat_round)+'.grd'),
+                cmap=data_dir + 'Figures/qs.cpt',
+                projection='x4.0/0.06',
+                region=[lonmin,lonmax,depmax,depmin],
+                interpolation='n')
+    fig4.basemap(projection='x4.0/0.06',
+                region=[lonmin,lonmax,depmax,depmin],
+                frame=['WSne','xa1f0.5+lLongitude','ya10f10+l"Depth (km)"'],
+                )
+    fig4.colorbar(frame=['xc' + data_dir + 'Figures/cbar_annots.txt+LQs'],
+                 cmap=data_dir + 'Figures/qs.cpt',
+                 position='JMR+o0.5c/0c+w4.92c/0.4c+n"No Data"')
+    fig4.savefig((data_dir + 'Figures/'+str(lat_round)+'initial_slice.png'))
